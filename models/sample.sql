0b6bcf0d0f0038369ade95fc3dade0f5f33ece28
Jeffrey Ngugi <ngugijeffrey@gmail.com>
2023-07-06 21:09:03 +0300

    Commit Request for a sample model

--- models/sample.sql
+++ models/sample.sql
@@ -1,2 +1,12 @@
-SELECT *
-FROM Table
+SELECT ID,
+    NAME,
+    NATIONALITY,
+    CREATED_AT as users_created_at,
+    LANGUAGE,
+    GENDER,
+    OCCUPATION,
+    ACADEMIC_DEGREE,
+    address:city::string as CITY,
+    address:province::string as PROVINCE,
+    address:country_code::string as COUNTRY_CODE
+FROM {{ source('airbyte_pull','users')}}
